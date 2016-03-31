7a2ef92fca1e22aab0c798b65eda00227b280b02
Steve Dower <steve.dower@microsoft.com>
2016-03-31 10:56:11 -0700

    Delete deploy_credentials.py

--- deploy_credentials.py
+++
@@ -1,5 +0,0 @@
-from azure.common.credentials import UserPassCredentials
-
-CREDENTIALS = UserPassCredentials('builddemo@stevedowerlive.onmicrosoft.com', 'Boco2913')
-
-SUBSCRIPTION_ID = 'b34fbd49-4655-4d16-930a-d8f88e03d10c'
